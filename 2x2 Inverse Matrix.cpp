b5a41e6cd7db70e868cd94c0f6bf9325c0788605
A_Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-02 16:46:34 -0700

    Create 2x2 Inverse Matrix.cpp

---
+++ 2x2 Inverse Matrix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+// Invert 2x2 matrix, with indices a, b, c, d
+
+double inv(double a, double b, double c, double d){
+    double m11 = d * (1/((a * d)- (b * c)));
+    double m12 = - b * (1/((a * d)- (b * c)));
+    double m21 = -c * (1/((a * d)- (b * c)));
+    double m22 = a * (1/((a * d)- (b * c)));
+std::cout << "[" << m11 << " " << m12 << " / " << m21 << " " << m22 << "]\n";
+    return m11;
+    return m12;
+    return m21;
+    return m22;
+    
+}
+
+int main(){   // Main function, takes in argc and argv
+
+inv(1, 8, 3, 4);
+return 0;
+
+}
