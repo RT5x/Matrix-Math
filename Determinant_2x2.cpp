a3e2f6827b9f32f822be5ccf9ccdbf76ad3f6662
A_Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-02 16:27:41 -0700

    Update Determinant_2x2.cpp

--- Determinant_2x2.cpp
+++ Determinant_2x2.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 // Determinant 2x2
-// Square matrix with  indices a, b, c, d
+// 2x2 square matrix with  indices a, b, c, d
 int det(int a, int b, int c, int d){
     int result = (a * d) - (b * c);
     return result;
