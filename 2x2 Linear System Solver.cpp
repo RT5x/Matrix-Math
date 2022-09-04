a2d563569db8cb5a8503bc101def6e4a8e8790be
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-09-04 10:14:31 -0700

    Create 2x2 Linear System Solver.cpp

---
+++ 2x2 Linear System Solver.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+// x + y = 1
+// 2x - 3y = 5
+
+// [1  1] [x] = [1]
+// [2 -3] [y] = [5]
+
+//[x, y] = A^-1 * [1, 5]
+/*  
+[a  b] [x] = [p]
+[c  d] [y] = [z]
+*/
+/*
+[x] = [m11 m12] *  [p] = [p*m11 + z*m12]
+[y] = [m21 m22]    [z]   [p*m21 + z*m22]
+*/
+int main() {
+int a;
+double b;
+double c;
+double d;
+double z;
+double p;
+std::cout << "2x2 System Solver" << std::endl;
+std::cout << " " << std::endl;
+std::cout << "First x coefficient: " << std::endl;
+std::cin >> a;
+std::cout << "First equation y coefficient: " << std::endl;
+std::cin >> b;
+std::cout << "Second equation x coefficient: " << std::endl;
+std::cin  >> c;
+std::cout << "Second equation y coefficient: " << std::endl;
+std::cin  >> d;
+std::cout << "First equation equals: " << std::endl;
+std::cin >> p;
+std::cout << "Second equation equals: " << std::endl;
+std::cin >> z;
+
+
+double m11 {d * (1/((a * d)- (b * c)))};
+double m12 {- b * (1/((a * d)- (b * c)))};
+double m21 {-c * (1/((a * d)- (b * c)))};
+double m22 {a * (1/((a * d)- (b * c)))};
+double x {p*m11 + z*m12};
+double y {p*m21 + z*m22};
+std::cout << "   " << std::endl;
+std::cout << "System: " << std::endl;
+std::cout << a << "x + " << b << "y = " << p << std::endl;
+std::cout << c << "x + " << d << "y = " << z << std::endl;
+std::cout << "   " << std::endl;
+std::cout << "Solution: " << std::endl;
+std::cout << "x = " << x << std::endl;
+std::cout << "y = " << y << std::endl;
+}
